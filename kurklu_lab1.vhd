9b14181d4266b732bd222926d37c23e57520ce41
Ahmet KURKLU <ahkurklu>
2023-01-03 17:17:13 +0100

    Ajout pio dans .qsys et ajout led dans .vhdl

--- kurklu_lab1.vhd
+++ kurklu_lab1.vhd
@@ -5,13 +5,15 @@ use ieee.numeric_std.all;
 entity kurklu_lab1 is
 	port(
 		clk : in std_logic := '0';
-		reset_n : in std_logic :='0');
+		reset_n : in std_logic :='0';
+		led : out std_logic_vector(7 downto 0));
 end entity kurklu_lab1;
 
 architecture rit of kurklu_lab1 is
 	component kurklu_lab1_sys is
         port (
             clk_clk       : in std_logic := 'X'; -- clk
+				pio_0_external_connection_export : out std_logic_vector(7 downto 0);        -- export
             reset_reset_n : in std_logic := 'X'  -- reset_n
         );
     end component kurklu_lab1_sys;
@@ -20,6 +22,7 @@ begin
 	u0 : component kurklu_lab1_sys
         port map (
             clk_clk       => clk,       --   clk.clk
+				pio_0_external_connection_export => led, -- pio_0_external_connection.export
             reset_reset_n => reset_n-- reset.reset_n
         );
 		  
